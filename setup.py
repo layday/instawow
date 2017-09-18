b4b11ddc9f3da3b4a10d7bb51e93b40f54993682
layday <layday@protonmail.com>
2017-09-18 15:39:36 +0300

    Remove test/obsolete dependencies from setup script

--- setup.py
+++ setup.py
@@ -18,8 +18,5 @@ setup(name='instawow',
                         'lxml>=3.8.0,<4', 'pydantic>=0.4,<1',
                         'SQLAlchemy>=1.1.13,<2', 'texttable>=0.9.1,<1',
                         'uvloop>=0.8.0,<1', 'yarl>=0.12.0,<1'],
-      extras_require={'test': ['pytest>=3.2.1,<4', 'vcrpy>=1.11.1,<2']},
-      dependency_links=['git+https://bitbucket.org/astanin/python-tabulate@feature/multiline-rows#egg=tabulate-0.8.0',
-                        'git+https://github.com/kevin1024/vcrpy@c3ecf8c#egg=vcrpy-1.11.1'],
       entry_points={'console_scripts': ['instawow = instawow.cli:cli',
                                         'instawow-init = instawow.cli:init']})
