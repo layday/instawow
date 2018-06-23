0d0b14c729f7b5dd7bd08f39fb27882bf11047f9
layday <layday@protonmail.com>
2018-06-23 12:38:16 +0300

    Display progress bar for large downloads

--- instawow/manager.py
+++ instawow/manager.py
@@ -4,7 +4,7 @@ import contextvars
 from functools import partial
 import typing as T
 
-from aiohttp import ClientSession, TCPConnector
+from aiohttp import ClientSession, TCPConnector, TraceConfig
 from send2trash import send2trash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -18,8 +18,6 @@ from .utils import Archive
 
 
 _UA_STRING = 'instawow (https://github.com/layday/instawow)'
-
-tqdm = partial(_tqdm, leave=False, ascii=True)
 
 
 def init_loop():
@@ -227,6 +225,48 @@ class Manager:
         raise NotImplementedError
 
 
+tqdm = partial(_tqdm, leave=False, ascii=True)
+
+_dl_counter = contextvars.ContextVar('_dl_counter', default=0)
+
+
+def _post_increment_dl_counter():
+    val = _dl_counter.get()
+    _dl_counter.set(val + 1)
+    return val
+
+
+async def _init_cli_client(*, loop):
+    async def do_on_request_end(session, context, params):
+        if (params.response.content_length and
+                # Ignore files smaller than a megabyte
+                params.response.content_length > 2**20):
+            bar = tqdm(total=params.response.content_length,
+                       desc=f'Downloading {params.response.url.name}',
+                       miniters=1, unit='B', unit_scale=True,
+                       position=_post_increment_dl_counter())
+
+            async def ticker():
+                while True:
+                    if params.response.content._cursor == bar.total:
+                        bar.close()
+                        break
+                    bar.update(params.response.content._cursor - bar.n)
+                    # The polling frequency's gotta be high
+                    # (higher than the ``tqdm.mininterval`` default)
+                    # so this bar gets to flush itself down the proverbial
+                    # drain before ``CliManager.gather``'s bar or it's gonna
+                    # leave behind an empty line which would be, truly,
+                    # devastating
+                    await asyncio.sleep(.01)
+            loop.create_task(ticker())
+
+    trace_conf = TraceConfig()
+    trace_conf.on_request_end.append(do_on_request_end)
+    trace_conf.freeze()
+    return await _init_client(loop=loop, trace_configs=[trace_conf])
+
+
 async def _intercept_exc_async(index, awaitable):
     try:
         result = await awaitable
@@ -253,13 +293,14 @@ class CliManager(Manager):
                  config: Config, loop: asyncio.BaseEventLoop=None,
                  show_progress: bool=True):
         self.show_progress = show_progress
-        super().__init__(config=config, loop=loop)
+        super().__init__(config=config, loop=loop,
+                         client_factory=_init_cli_client if show_progress else None)
 
     async def gather(self, it: T.Iterable, **kwargs) -> list:
         futures = [_intercept_exc_async(*i) for i in enumerate(it)]
         results = [None] * len(futures)
         with tqdm(total=len(futures), disable=not self.show_progress,
-                  **kwargs) as bar:
+                  position=_post_increment_dl_counter(), **kwargs) as bar:
             for result in asyncio.as_completed(futures, loop=self.loop):
                 results.__setitem__(*await result)
                 bar.update(1)
