0c0763995d84036605e14b3e939da5a4d95a41ec
layday <layday@protonmail.com>
2019-01-27 22:06:47 +0200

    Make profile switching possible

--- instawow/config.py
+++ instawow/config.py
@@ -1,42 +1,45 @@
 
-from os import environ
 from pathlib import Path
-import typing as T
 
 import click
 import pydantic
 
 
-_DEFAULT_CONFIG_DIR = Path(click.get_app_dir('instawow'))
+DEFAULT_CONFIG_DIR = Path(click.get_app_dir('instawow'))
 
 
 class Config(pydantic.BaseSettings):
 
+    config_dir: Path
     addon_dir: Path
-    config_dir: Path = _DEFAULT_CONFIG_DIR
 
     @pydantic.validator('addon_dir')
-    def _prepare_addon_dir(cls, value: Path) -> Path:
+    def expand_and_validate_addon_dir(cls, value: Path) -> Path:
         value = value.expanduser().resolve()
         if not value.is_dir():
-            raise ValueError
+            raise ValueError(f'{value} is not a directory')
         return value
 
-    @pydantic.validator('config_dir')
-    def _prepare_config_dir(cls, value: Path) -> Path:
-        value.mkdir(exist_ok=True)
-        return value
+    def write(self) -> None:
+        "Create the profile."
+        self.config_dir.mkdir(exist_ok=True)
+
+    class Config:
+        env_prefix = 'INSTAWOW_'
 
 
 class UserConfig(Config):
 
+    config_dir: Path = DEFAULT_CONFIG_DIR
+
     @classmethod
     def read(cls):  # -> UserConfig
-        "Attempt to read the config from the default path."
-        return cls(addon_dir=(_DEFAULT_CONFIG_DIR/'addon_dir.txt')
+        "Attempt to read ``addon_dir`` from disk."
+        return cls(addon_dir=(cls(addon_dir='').config_dir/'addon_dir.txt')
                              .read_text(encoding='utf-8'))
 
     def write(self) -> None:
-        "Write ``self.addon_dir`` on disk."
+        "Create the profile and write ``addon_dir`` on disk."
+        super().write()
         (self.config_dir/'addon_dir.txt').write_text(str(self.addon_dir),
                                                      encoding='utf-8')
