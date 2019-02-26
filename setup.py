abf5e98e9e4160c27d45ce14f82f7c5962c5563b
layday <layday@protonmail.com>
2019-02-26 20:26:48 +0200

    Update setup logic

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@
 from pathlib import Path
 from setuptools import setup
 
+try:
+    import fastentrypoints
+except ImportError:
+    pass
+
 from instawow import __version__
 
 
@@ -11,23 +16,10 @@ setup(name='instawow',
       url='http://github.com/layday/instawow',
       author='layday',
       author_email='layday@protonmail.com',
-      license='GPLv3',
+      license='GPL-3.0-or-later',
       long_description=Path('README.rst').read_text(),
       python_requires='~=3.7',
       packages=['instawow'],
-      install_requires=['aiohttp==3.5.4',
-                        'click==7.0',
-                        'jinja2==2.10',
-                        'Logbook==1.4.3',
-                        'luaparser==2.1.2',
-                        'parsel==1.5.1',
-                        'pydantic==0.19',
-                        'Send2Trash==1.5.0',
-                        'SQLAlchemy==1.2.17',
-                        'texttable==1.6.0',
-                        'tqdm==4.31.1',
-                        'uvloop==0.12.1; platform_system != "Windows"',
-                        'yarl==1.3.0'],
-      package_data={'instawow': ['assets/*',
-                                 'wa_templates/*']},
+      install_requires=Path('requirements.txt').read_text(),
+      include_package_data=True,
       entry_points={'console_scripts': ['instawow = instawow.cli:cli']})
