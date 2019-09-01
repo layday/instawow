d6b9f7391154e89297b9f61cd98d5eb6fc066d33
layday <layday@protonmail.com>
2019-09-01 13:07:20 +0300

    Unbork profiles after migration to Pydantic

--- instawow/config.py
+++ instawow/config.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 __all__ = ('Config',)
 
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Union
 
 import click
 import pydantic
@@ -16,7 +16,11 @@ except ImportError:
 
 
 _my_path = Path(__file__)
-_default_config_dir = lambda: Path(click.get_app_dir('instawow'))
+_default_config_dir = lambda: click.get_app_dir('instawow')
+
+
+def _expand_path(value: Union[str, Path]) -> Path:
+    return Path(value).expanduser().resolve()
 
 
 class _Config(pydantic.BaseSettings):
@@ -28,14 +32,14 @@ class _Config(pydantic.BaseSettings):
     game_flavour: Literal['retail', 'classic']
 
     @pydantic.validator('config_dir', always=True, pre=True)
-    def __ensure_config_dir(cls, value: Path) -> Path:
+    def __ensure_config_dir(cls, value: Union[str, Path]) -> Path:
         if value == _my_path:
             value = _default_config_dir()
-        return value
+        return _expand_path(value)
 
     @pydantic.validator('addon_dir')
     def __transform_addon_dir(cls, value: Path) -> Path:
-        value = Path(value).expanduser().resolve()
+        value = _expand_path(value)
         if not value.is_dir():
             raise ValueError('folder does not exist')
         return value
@@ -56,9 +60,9 @@ class _Config(pydantic.BaseSettings):
         return super().json(exclude={'config_dir'}, indent=2)
 
     @classmethod
-    def read(cls, config_dir: Optional[Path] = None) -> _Config:
-        config_dir = config_dir or _default_config_dir()
-        return cls.parse_raw((config_dir / 'config.json')
+    def read(cls) -> _Config:
+        dummy_config = cls(addon_dir='', game_flavour='retail')
+        return cls.parse_raw((dummy_config.config_dir / 'config.json')
                              .read_text(encoding='utf-8'))
 
     def write(self) -> _Config:
