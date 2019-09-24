c1b6defc89aead4129de9ab05958a6b1ce4bfbfd
layday <layday@protonmail.com>
2019-09-24 09:17:45 +0300

    Fix installing in non-UTF-8 locale
    Closes #24.

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+from functools import partial
 import os
 from pathlib import Path
 from setuptools import find_packages, setup
 
 if os.environ.get('INSTAWOW_DEV'):
     import fastentrypoints
+
+
+read_text = partial(Path.read_text, encoding='utf-8')
 
 
 setup(name='instawow',
@@ -14,10 +18,10 @@ setup(name='instawow',
       author='layday',
       author_email='layday@protonmail.com',
       license='GPL-3.0-or-later',
-      long_description=Path('README.rst').read_text(),
+      long_description=read_text(Path('README.rst')),
       python_requires='~=3.7',
       packages=find_packages(),
       setup_requires='setuptools_scm',
-      install_requires=Path('requirements.txt').read_text(),
+      install_requires=read_text(Path('requirements.txt')),
       include_package_data=True,
       entry_points={'console_scripts': ['instawow = instawow.cli:main']},)
