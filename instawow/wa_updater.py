b75dc01328f732205cb99780538643b05db4d812
layday <layday@protonmail.com>
2019-09-25 13:46:35 +0300

    Bump TOC version for WA Companion

--- instawow/wa_updater.py
+++ instawow/wa_updater.py
@@ -195,7 +195,7 @@ class WaCompanionBuilder(ManagerAttrAccessMixin):
                        'stash': []})    # Send to WAC not supported - always empty
             write_tpl('init.lua', {})
             write_tpl('WeakAurasCompanion.toc',
-                      {'interface': '11302' if self.config.is_classic else '80200'})
+                      {'interface': '11302' if self.config.is_classic else '80205'})
 
     async def build(self, account: Optional[str] = None) -> None:
         config = BuilderConfig(account=account)
