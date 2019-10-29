1e0e7880d61fceb65f7fe16ad6307496b9f72d05
layday <layday@protonmail.com>
2019-10-29 13:47:27 +0200

    Get download bar label from request ctx before params

--- instawow/manager.py
+++ instawow/manager.py
@@ -375,7 +375,9 @@ async def init_cli_web_client(*, manager: CliManager) -> aiohttp.ClientSession:
                                 ctx: SimpleNamespace,
                                 params: aiohttp.TraceRequestEndParams) -> None:
         if ctx.trace_request_ctx and ctx.trace_request_ctx.get('show_progress'):
-            bar = manager.bar(label=f'Downloading {extract_filename(params)}',
+            label = (ctx.trace_request_ctx.get('label')
+                     or f'Downloading {extract_filename(params)}')
+            bar = manager.bar(label=label,
                               total=(params.response.content_length or 0))
 
             async def ticker(bar=bar, params=params) -> None:
