c225a4601bc014ee5f69211177e8e229a23958b0
layday <layday@protonmail.com>
2020-10-14 05:46:30 +0300

    Update noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,34 +1,19 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 import nox
-
-if TYPE_CHECKING:
-    from nox.sessions import Session
-
 
 nox.options.envdir = '.py-nox'
 
 
 @nox.session(python=['3.7', '3.8', '3.9'])
-def test(session: Session):
+def test(session: nox.Session):
     session.install('.[server, test]')
     session.run('coverage', 'run', '-m', 'pytest')
     session.run('coverage', 'report', '-m')
 
 
-@nox.session(python=False)
-def update_typeshed(session: Session):
-    types_dir = '.py-types'
-    session.run('rm', '-rf', types_dir)
-    session.run(
-        *f'git clone --depth 1 https://github.com/python/typeshed {types_dir}/typeshed'.split()
-    )
-
-
 @nox.session(python=['3.7', '3.8', '3.9'])
-def type_check(session: Session):
+def type_check(session: nox.Session):
     # The instawow path is hardcoded in pyrightconfig.json relative
     # to the enclosing folder therefore we can't install instawow in a
     # virtual environment or Pyright won't be able to find it.
@@ -40,7 +25,7 @@ def type_check(session: Session):
     # and is imported in ``utils.py``.
     tmp_dir = session.create_tmp()
     session.run('git', 'clone', '.', tmp_dir)
-    session.cd(tmp_dir)
+    session.chdir(tmp_dir)
     session.install(
         '-e',
         '.[server]',
@@ -49,27 +34,27 @@ def type_check(session: Session):
     session.run('npx', '--cache', '.npm', 'pyright')
 
 
-@nox.session(python='3.7', reuse_venv=True)
-def reformat(session: Session):
+@nox.session(reuse_venv=True)
+def reformat(session: nox.Session):
     session.install('isort>=5.5.2', 'black>=20.8b1')
     for cmd in ('isort', 'black'):
         session.run(cmd, 'instawow', 'tests', 'noxfile.py', 'setup.py')
 
 
 @nox.session(python=False)
-def clobber_build_artefacts(session: Session):
+def clobber_build_artefacts(session: nox.Session):
     session.run('rm', '-rf', 'build', 'dist', 'instawow.egg-info')
 
 
-@nox.session(python='3.7')
-def build(session: Session):
+@nox.session(python='3.8')
+def build(session: nox.Session):
     clobber_build_artefacts(session)
     session.install('build>=0.0.4')
     session.run('python', '-m', 'build', '.')
 
 
-@nox.session(python='3.7')
-def publish(session: Session):
+@nox.session
+def publish(session: nox.Session):
     session.install('twine')
     for subcmd in ('check', 'upload'):
         session.run('twine', subcmd, 'dist/*')
