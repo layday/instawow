fd74e725a17065f9ff6e3864a6d5dc0e5471319b
layday <layday@protonmail.com>
2020-12-04 22:54:29 +0200

    wac: bump classic TOC

--- instawow/wa_updater.py
+++ instawow/wa_updater.py
@@ -315,7 +315,7 @@ class WaCompanionBuilder:
             write_tpl('init.lua', {})
             write_tpl(
                 'WeakAurasCompanion.toc',
-                {'interface': '11305' if self.manager.config.is_classic else '90002'},
+                {'interface': '11306' if self.manager.config.is_classic else '90002'},
             )
 
     async def build(self) -> None:
