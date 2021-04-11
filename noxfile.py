19afc7c1597e01d791e0bd50b8d539cb29c97559
layday <layday@protonmail.com>
2021-04-11 14:52:10 +0300

    Temporarily pin Pyright

--- noxfile.py
+++ noxfile.py
@@ -90,7 +90,7 @@ def type_check(session: nox.Session):
         '.[server]',
         'sqlalchemy-stubs@ https://github.com/layday/sqlalchemy-stubs/archive/develop.zip',
     )
-    session.run('npx', 'pyright')
+    session.run('npx', 'pyright@1.1.129')
 
 
 @nox.session(python=False)
