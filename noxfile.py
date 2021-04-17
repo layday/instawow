015a0a13711b1522435a024c1cb6efa2d6dd478b
layday <layday@protonmail.com>
2021-04-17 05:58:01 +0300

    Revert "Temporarily pin Pyright"
    This reverts commit 19afc7c1597e01d791e0bd50b8d539cb29c97559.

--- noxfile.py
+++ noxfile.py
@@ -90,7 +90,7 @@ def type_check(session: nox.Session):
         '.[server]',
         'sqlalchemy-stubs@ https://github.com/layday/sqlalchemy-stubs/archive/develop.zip',
     )
-    session.run('npx', 'pyright@1.1.129')
+    session.run('npx', 'pyright')
 
 
 @nox.session(python=False)
