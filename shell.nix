b76ff837377d8d1a92552466e7243617b37bcddc
layday <layday@protonmail.com>
2021-06-12 03:36:40 +0300

    Use `frontend_editables` in development

--- shell.nix
+++ shell.nix
@@ -16,7 +16,11 @@ pkgs.mkShell {
   PYTHONBREAKPOINT = "IPython.terminal.debugger.set_trace";
 
   postVenvCreation = ''
-    python -m pip install -U pip setuptools
-    python -m pip install -U ipython nox '.[gui, test, types]'
+    python -m pip install -U pip setuptools ipython nox
+    python -m pip install git+https://github.com/layday/frontend-editables
+    python -m frontend_editables.transitional_cli \
+      --spec ".[gui, test, types]" \
+      src/instawow instawow \
+      gui-webview/src/instawow_gui instawow_gui
   '';
 }
