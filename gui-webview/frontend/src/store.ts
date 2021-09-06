6393ec260355069b2b87668a8df7a5df46e51faa
layday <layday@protonmail.com>
2021-09-06 18:50:28 +0300

    gui: remove old code

--- gui-webview/frontend/src/store.ts
+++ gui-webview/frontend/src/store.ts
@@ -1,7 +1,5 @@
 import type { Api, Config, Profile } from "./api";
 import { writable } from "svelte/store";
 
-// export const notifications = writable<Array<InAppNotification>>([]);
-
 export const profiles = writable<Map<Profile, Config>>(new Map());
 export const activeProfile = writable<Profile | undefined>(undefined);
