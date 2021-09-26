4bfd22ea059e42b73a33de5735746e9c556d8fc3
layday <layday@protonmail.com>
2021-09-26 15:34:01 +0300

    Prevent using 'deferred' types

--- src/instawow/_deferred_types.py
+++ src/instawow/_deferred_types.py
@@ -1,4 +1,7 @@
 # pyright: reportUnusedImport=false
 
-import aiohttp
-import prompt_toolkit.shortcuts
+import typing as _typing
+
+if _typing.TYPE_CHECKING:
+    import aiohttp
+    import prompt_toolkit.shortcuts
