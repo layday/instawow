9bbb43912c664e48c4206f34a01d54f864f44c27
layday <layday@protonmail.com>
2021-12-22 14:20:01 +0200

    Fix Poetry build in devel
    https://github.com/pypa/setuptools/issues/2939#issuecomment-999040464

--- shell.nix
+++ shell.nix
@@ -16,9 +16,8 @@ pkgs.mkShell {
   PYTHONBREAKPOINT = "IPython.terminal.debugger.set_trace";
 
   postVenvCreation = ''
-    python -m pip uninstall -y setuptools
     python -m pip install -U pip ipython nox frontend-editables
-    python -m frontend_editables.transitional_cli \
+    SETUPTOOLS_USE_DISTUTILS=0 python -m frontend_editables.transitional_cli \
       --method lax_symlink --spec ".[gui, test, types]" \
       src/instawow instawow \
       gui-webview/src/instawow_gui instawow_gui
