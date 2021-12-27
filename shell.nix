884df7d8fbbde9be0167833895a0dc28de4ec325
layday <layday@protonmail.com>
2021-12-28 01:02:24 +0200

    Only install from binaries

--- shell.nix
+++ shell.nix
@@ -14,9 +14,11 @@ pkgs.mkShell {
   VIRTUAL_ENV_DISABLE_PROMPT = "1";
   SOURCE_DATE_EPOCH = "315532800"; # The year 1980
   PYTHONBREAKPOINT = "IPython.terminal.debugger.set_trace";
+  PIP_ONLY_BINARY = ":all:";
 
   postVenvCreation = ''
     python -m pip install -U pip ipython nox frontend-editables
+    python -m pip uninstall -y setuptools
     SETUPTOOLS_USE_DISTUTILS=0 python -m frontend_editables.transitional_cli \
       --method lax_symlink --spec ".[gui, test, types]" \
       src/instawow instawow \
