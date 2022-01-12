d7a93de610a1b7fa524a36346e9cb01a643c1b7a
layday <layday@protonmail.com>
2022-01-12 23:40:07 +0200

    gui: bump `string-strip-html`
    Need to replace `process.env` from `tiny-invariant` when compiling.
    Annoyingly.

--- gui-webview/frontend/rollup.config.js
+++ gui-webview/frontend/rollup.config.js
@@ -1,6 +1,7 @@
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
 import typescript from "@rollup/plugin-typescript";
 import copy from "rollup-plugin-copy";
 import css from "rollup-plugin-css-only";
@@ -65,6 +66,10 @@ export default [
         inlineSources: !PRODUCTION,
       }),
       commonjs(),
+      replace({
+        preventAssignment: true,
+        values: { "process.env.NODE_ENV": JSON.stringify("production") },
+      }),
       json(),
       !PRODUCTION && livereload(BUILD_DIR),
       PRODUCTION && terser(),
