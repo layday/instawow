4af862fa9882e498a6e2b23e6910d904396f7985
layday <layday@protonmail.com>
2022-04-17 17:29:34 +0300

    Update WAC assets from upstream

--- src/instawow/wa_templates/init.lua
+++ src/instawow/wa_templates/init.lua
@@ -1,42 +1,44 @@
 -- file generated automatically
-local buildTimeTarget = 20190123023201
-local waBuildTime = tonumber(WeakAuras and WeakAuras.buildTime or 0)
+local loadedFrame = CreateFrame("FRAME")
+loadedFrame:RegisterEvent("ADDON_LOADED")
+loadedFrame:SetScript("OnEvent", function(_, _, addonName)
+  if addonName == "WeakAurasCompanion" then
+    timestamp = GetTime()
 
-if waBuildTime and waBuildTime > buildTimeTarget then
-  local loadedFrame = CreateFrame("FRAME")
-  loadedFrame:RegisterEvent("ADDON_LOADED")
-  loadedFrame:SetScript("OnEvent", function(_, _, addonName)
-    if addonName == "WeakAurasCompanion" then
-      local count = WeakAuras.CountWagoUpdates()
-      if count and count > 0 then
-        WeakAuras.prettyPrint(WeakAuras.L["There are %i updates to your auras ready to be installed!"]:format(count))
-      end
-      if WeakAuras.ImportHistory then
-        for id, data in pairs(WeakAurasSaved.displays) do
-          if data.uid and not WeakAurasSaved.history[data.uid] then
-            local slug = WeakAurasCompanion.uids[data.uid]
-            if slug then
-              local wagoData = WeakAurasCompanion.slugs[slug]
-              if wagoData and wagoData.encoded then
-                WeakAuras.ImportHistory(wagoData.encoded)
+    if WeakAuras and WeakAurasCompanion then
+      local WeakAurasData = WeakAurasCompanion.WeakAuras
+      if WeakAurasData then
+        local count = WeakAuras.CountWagoUpdates()
+        if count and count > 0 then
+          WeakAuras.prettyPrint(WeakAuras.L["There are %i updates to your auras ready to be installed!"]:format(count))
+        end
+        if WeakAuras.ImportHistory then
+          for id, data in pairs(WeakAurasSaved.displays) do
+            if data.uid and not WeakAurasSaved.history[data.uid] then
+              local slug = WeakAurasData.uids[data.uid]
+              if slug then
+                local wagoData = WeakAurasData.slugs[slug]
+                if wagoData and wagoData.encoded then
+                  WeakAuras.ImportHistory(wagoData.encoded)
+                end
               end
             end
           end
         end
-      end
-      if WeakAurasCompanion.stash then
-        local emptyStash = true
-        for _ in pairs(WeakAurasCompanion.stash) do
-          emptyStash = false
-        end
-        if not emptyStash and WeakAuras.StashShow then
-          C_Timer.After(5, function() WeakAuras.StashShow() end)
+        if WeakAurasData.stash then
+          local emptyStash = true
+          for _ in pairs(WeakAurasData.stash) do
+            emptyStash = false
+          end
+          if not emptyStash then
+            WeakAuras.prettyPrint(WeakAuras.L["You have new auras ready to be installed!"])
+          end
         end
       end
     end
-  end)
-end
 
-if Plater and Plater.CheckWagoUpdates then
-    Plater.CheckWagoUpdates()
-end
+    if Plater and Plater.CheckWagoUpdates then
+      Plater.CheckWagoUpdates()
+    end
+  end
+end)
