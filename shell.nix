131b88f8b5fabac3e0114c792cc2ad5fa1fac7d9
layday <layday@protonmail.com>
2022-10-08 15:39:46 +0300

    House nox session under venv dir

--- shell.nix
+++ shell.nix
@@ -1,20 +1,24 @@
 { pkgs ? import <nixpkgs> { } }:
 let
   python = pkgs.python310;
+  venvDir = toString ./venvs + ("/" + python.pythonVersion);
 in
 pkgs.mkShell {
   buildInputs = [
+    pkgs.nixpkgs-fmt
+    pkgs.nodejs-16_x
+    pkgs.rnix-lsp
     python
     python.pkgs.venvShellHook
-    pkgs.nodejs-16_x
   ];
 
-  venvDir = toString ./venvs + ("/" + python.pythonVersion);
-
+  NOX_ENVDIR = venvDir + "/.nox";
   VIRTUAL_ENV_DISABLE_PROMPT = "1";
   SOURCE_DATE_EPOCH = "315532800"; # The year 1980
   PYTHONBREAKPOINT = "IPython.terminal.debugger.set_trace";
   PIP_ONLY_BINARY = ":all:";
+
+  inherit venvDir;
 
   postVenvCreation = ''
     python -m pip install -U pip ipython nox
