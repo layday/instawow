228cd1624cab692a97c6a7c88de90407ee95c7c4
layday <layday@protonmail.com>
2023-07-19 10:58:05 +0300

    Revert "Fix type checking error"
    c0a0f63b9f0e33dc99f0a0a0d09cb785704d143a

--- src/instawow/cli.py
+++ src/instawow/cli.py
@@ -289,11 +289,7 @@ def _parse_debug_option(
 
 
 @_register_plugin_commands
-@click.group(
-    # Redundant; overload w/out type var default remains unsolved: https://github.com/pallets/click/issues/2558
-    cls=None,
-    context_settings={'help_option_names': ('-h', '--help')},
-)
+@click.group(context_settings={'help_option_names': ('-h', '--help')})
 @click.version_option(__version__, prog_name=__spec__.parent)
 @click.option(
     '--debug',
