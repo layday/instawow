73934211e6dfaa32b1b7eafc8ed060ad913ef716
layday <layday@protonmail.com>
2024-03-30 15:11:34 +0200

    Rm `sys.frozen` check

--- src/instawow/__init__.py
+++ src/instawow/__init__.py
@@ -1,10 +1,7 @@
 from __future__ import annotations
 
-import sys
-
 from . import _import_wrapper
 
-if not getattr(sys, 'frozen', False):
-    __getattr__ = _import_wrapper.__getattr__
+__getattr__ = _import_wrapper.__getattr__
 
 from ._version import __version__ as __version__
