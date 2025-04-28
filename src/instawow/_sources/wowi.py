4880a3d80b42befed95278541a1cf37fda43c74b
layday <layday@protonmail.com>
2025-04-28 16:40:20 +0100

    Avoid downloading the WOWI catalogue on resolve

--- src/instawow/_sources/wowi.py
+++ src/instawow/_sources/wowi.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import re
-from collections.abc import Collection, Sequence
+from collections.abc import Sequence
 from datetime import UTC, datetime, timedelta
 from itertools import takewhile
 from typing import Literal
@@ -10,19 +10,16 @@ from typing import NotRequired as N
 from typing_extensions import TypedDict
 from yarl import URL
 
-from .. import http_ctx, sync_ctx
+from .. import http_ctx
 from .._logging import logger
 from .._utils.aio import gather
 from .._utils.iteration import uniq
 from .._utils.text import normalise_names
 from .._utils.web import as_plain_text_data_url
 from ..definitions import ChangelogFormat, Defn, SourceMetadata
-from ..progress_reporting import make_download_progress
 from ..resolvers import BaseResolver, CatalogueEntryCandidate, PkgCandidate
 from ..results import PkgNonexistent, resultify
 from ..wow_installations import Flavour, FlavourVersionRange
-
-_LOAD_WOWI_CATALOGUE_LOCK = (object(), '_LOAD_WOWI_CATALOGUE_')
 
 _slugify = normalise_names('-')
 
@@ -34,11 +31,6 @@ class _WowiCommonTerms(TypedDict):
     UIDate: int  # Upload date expressed as unix epoch
     UIName: str  # User-facing add-on name
     UIAuthorName: str
-
-
-class _WowiListApiItem_CompatibilityEntry(TypedDict):
-    version: str  # Game version, e.g. '8.3.0'
-    name: str  # Xpac or patch name, e.g. "Visions of N'Zoth" for 8.3.0
 
 
 class _WowiListApiItem(_WowiCommonTerms):
@@ -58,6 +50,11 @@ class _WowiListApiItem(_WowiCommonTerms):
     UIDonationLink: N[str | None]  # Absent from the first item on the list (!)
 
 
+class _WowiListApiItem_CompatibilityEntry(TypedDict):
+    version: str  # Game version, e.g. '8.3.0'
+    name: str  # Xpac or patch name, e.g. "Visions of N'Zoth" for 8.3.0
+
+
 class _WowiDetailsApiItem(_WowiCommonTerms):
     UIMD5: str | None  # Archive hash, ``null` when review pending
     UIFileName: str  # The actual filename, e.g. 'foo.zip'
@@ -69,15 +66,11 @@ class _WowiDetailsApiItem(_WowiCommonTerms):
     UIHitCountMonthly: str  # Same as UIDownloadMonthly
 
 
-class _WowiCombinedItem(_WowiListApiItem, _WowiDetailsApiItem):
-    pass
-
-
 def _timestamp_to_datetime(timestamp: int):
     return datetime.fromtimestamp(timestamp / 1000, UTC)
 
 
-class WowiResolver(BaseResolver[_WowiCombinedItem]):
+class WowiResolver(BaseResolver[_WowiDetailsApiItem]):
     metadata = SourceMetadata(
         id='wowi',
         name='WoWInterface',
@@ -86,17 +79,23 @@ class WowiResolver(BaseResolver[_WowiCombinedItem]):
         addon_toc_key='X-WoWI-ID',
     )
 
-    # Reference: https://api.mmoui.com/v3/globalconfig.json
-    # There's also a v4 API corresponding to the as yet unreleased Minion v4,
-    # which is fair to assume is unstable.  They changed the naming scheme to
-    # camelCase and some fields which were strings were converted to numbers.
+    # Ref: https://api.mmoui.com/v3/globalconfig.json
+    # There's also a v4 API corresponding to the unreleased Minion v4,
+    # which is fair to assume is unstable.  The naming scheme was changed to
+    # camel case and some fields which contained strings were turned into numbers.
     # Neither API provides access to classic files for multi-file add-ons and
-    # 'UICompatibility' can't be relied on to enforce compatibility
-    # in instawow.  The API appears to inherit the version of the latest
+    # ``UICompatibility`` can't be relied on to enforce compatibility - not only
+    # because it's not consistently populated, but also because it's not
+    # reported by the details endpoint and keeping the two endpoints
+    # in sync is tricky.
+    # We could theoretically inspect the downloaded file for compatbility,
+    # but that's a lot of bandwidth to be wasting on add-ons which might
+    # not end up being installed.
+    # The API appears to inherit the version of the latest
     # file to have been uploaded, which for multi-file add-ons can be the
-    # classic version.  Hoooowever the download link always points to the
-    # 'retail' version, which for single-file add-ons belonging to the
-    # classic category would be an add-on for classic.
+    # classic version.  Hoooowever, the download link always points to the
+    # 'retail' version.
+    # In essence, the WoWI API is only good for installing retail add-ons.
     __list_api_url = 'https://api.mmoui.com/v3/game/WOW/filelist.json'
     __details_api_url = URL('https://api.mmoui.com/v3/game/WOW/filedetails/')
 
@@ -114,56 +113,25 @@ class WowiResolver(BaseResolver[_WowiCombinedItem]):
                 match = re.match(r'^(?:download|info)(?P<id>\d+)', url.name)
                 return match and match['id']
 
-    async def __get_addons(self):
-        async with sync_ctx.locks()[_LOAD_WOWI_CATALOGUE_LOCK]:
-            async with http_ctx.web_client().get(
-                self.__list_api_url,
-                expire_after=timedelta(hours=1),
-                raise_for_status=True,
-                trace_request_ctx={
-                    'progress': make_download_progress(
-                        label=f'Fetching {self.metadata.name} catalogue'
-                    )
-                },
-            ) as response:
-                return {i['UID']: i for i in await response.json()}
+    async def resolve(self, defns: Sequence[Defn]):
+        defn_ids = {d: ''.join(takewhile(str.isdigit, d.alias)) for d in defns}
 
-    async def __get_addons_details(self, ids: Collection[str]):
         async with http_ctx.web_client().get(
-            (self.__details_api_url / f'{",".join(uniq(i for i in ids if i))}.json'),
-            expire_after=timedelta(minutes=5),
-            trace_request_ctx={
-                'progress': make_download_progress(
-                    label=f'Fetching {self.metadata.name} add-on details'
-                )
-            },
+            (self.__details_api_url / f'{",".join(uniq(filter(None, defn_ids.values())))}.json'),
+            expire_after=timedelta(minutes=15),
         ) as response:
             if response.status == 404:
-                return None
+                return await super().resolve(defns)
 
             response.raise_for_status()
 
-            return {i['UID']: i for i in await response.json()}
-
-    async def resolve(self, defns: Sequence[Defn]):
-        defns_to_ids = {d: ''.join(takewhile(str.isdigit, d.alias)) for d in defns}
-
-        addons = await self.__get_addons()
-        addons_details = await self.__get_addons_details(defns_to_ids.values())
-        if addons_details is None:
-            return await super().resolve(defns)
+            addons_details = {i['UID']: i for i in await response.json()}
 
         resolve_one = resultify(self.resolve_one)
-        results = await gather(
-            resolve_one(
-                d,
-                a | b if (a := addons.get(i)) and (b := addons_details.get(i)) else None,
-            )
-            for d, i in defns_to_ids.items()
-        )
+        results = await gather(resolve_one(d, addons_details.get(i)) for d, i in defn_ids.items())
         return dict(zip(defns, results))
 
-    async def resolve_one(self, defn: Defn, metadata: _WowiCombinedItem | None):
+    async def resolve_one(self, defn: Defn, metadata: _WowiDetailsApiItem | None):
         if metadata is None:
             raise PkgNonexistent
 
@@ -172,7 +140,7 @@ class WowiResolver(BaseResolver[_WowiCombinedItem]):
             slug=_slugify(f'{metadata["UID"]} {metadata["UIName"]}'),
             name=metadata['UIName'],
             description=metadata['UIDescription'],
-            url=metadata['UIFileInfoURL'],
+            url=f'https://www.wowinterface.com/downloads/info{metadata["UID"]}',
             download_url=metadata['UIDownload'],
             date_published=_timestamp_to_datetime(metadata['UIDate']),
             version=metadata['UIVersion'],
