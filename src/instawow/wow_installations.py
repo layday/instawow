7fc77ee63e4f4ca1521531614d65164ec0a16431
layday <layday@protonmail.com>
2025-07-08 18:29:52 +0100

    Revert "Invert `Track` alias"
    This reverts commit 80ac80753489e9865d777de2b4b82bd768da530e.

--- src/instawow/wow_installations.py
+++ src/instawow/wow_installations.py
@@ -14,8 +14,8 @@ from ._utils.iteration import fill
 class Track(StrEnum):
     Retail = 'retail'
     VanillaClassic = 'vanilla_classic'
-    Classic = 'classic'
-    MistsClassic = Classic
+    MistsClassic = 'classic'
+    Classic = MistsClassic
 
 
 class Flavour(StrEnum):
