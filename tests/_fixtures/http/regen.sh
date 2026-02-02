c80a7bae322b58bd1f89254204ec47c36c5e87a7
layday <layday@protonmail.com>
2026-02-02 16:30:10 +0000

    Just use curl

--- tests/_fixtures/http/regen.sh
+++ tests/_fixtures/http/regen.sh
@@ -1,91 +1,94 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p httpie jq
+#! nix-shell -i bash -p curl jq
 
 set -ex
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 
 
-echo '{"modIds": [2382, 13592, 326516, 402180, 1299675]}' \
-    | http post https://api.curseforge.com/v1/mods \
-        x-api-key:$INSTAWOW_ACCESS_TOKENS_CFCORE -b \
+curl -X POST https://api.curseforge.com/v1/mods \
+    -H 'Content-Type: application/json' \
+    -H "x-api-key: $INSTAWOW_ACCESS_TOKENS_CFCORE" \
+    -d '{"modIds": [2382, 13592, 326516, 402180, 1299675]}' \
     | jq -r \
     > "$DIR"/curse-addon--all.json
-http get 'https://api.curseforge.com/v1/mods/search?gameId=1&slug=masque' \
-        x-api-key:$INSTAWOW_ACCESS_TOKENS_CFCORE -b \
+curl 'https://api.curseforge.com/v1/mods/search?gameId=1&slug=masque' \
+    -H "x-api-key: $INSTAWOW_ACCESS_TOKENS_CFCORE" \
     | jq -r \
     > "$DIR"/curse-addon-slug-search.json
-http get https://api.curseforge.com/v1/mods/13592/files \
-        x-api-key:$INSTAWOW_ACCESS_TOKENS_CFCORE -b \
+curl https://api.curseforge.com/v1/mods/13592/files \
+    -H "x-api-key: $INSTAWOW_ACCESS_TOKENS_CFCORE" \
     | jq -r \
     > "$DIR"/curse-addon-files.json
-http get https://api.curseforge.com/v1/mods/13592/files/7373575 \
-        x-api-key:$INSTAWOW_ACCESS_TOKENS_CFCORE -b \
+curl https://api.curseforge.com/v1/mods/13592/files/7373575 \
+    -H "x-api-key: $INSTAWOW_ACCESS_TOKENS_CFCORE" \
     | jq -r \
     > "$DIR"/curse-addon-file-7373575.json
-    http get https://api.curseforge.com/v1/mods/13592/files/7398127 \
-            x-api-key:$INSTAWOW_ACCESS_TOKENS_CFCORE -b \
+curl https://api.curseforge.com/v1/mods/13592/files/7398127 \
+    -H "x-api-key: $INSTAWOW_ACCESS_TOKENS_CFCORE" \
     | jq -r \
     > "$DIR"/curse-addon-file-7398127.json
-http get https://api.curseforge.com/v1/mods/13592/files/7398127/changelog \
-        x-api-key:$INSTAWOW_ACCESS_TOKENS_CFCORE -b \
+curl https://api.curseforge.com/v1/mods/13592/files/7398127/changelog \
+    -H "x-api-key: $INSTAWOW_ACCESS_TOKENS_CFCORE" \
     | jq -r \
     > "$DIR"/curse-addon-changelog.json
 
 
-http get https://api.mmoui.com/v3/game/WOW/filelist.json -b \
+curl https://api.mmoui.com/v3/game/WOW/filelist.json \
     | jq -r '.[] | select(.UID == "12097") | [.]' \
     > "$DIR"/wowi-filelist.json
-http get https://api.mmoui.com/v3/game/WOW/filedetails/12097.json -b \
+curl https://api.mmoui.com/v3/game/WOW/filedetails/12097.json \
     | jq -r \
     > "$DIR"/wowi-filedetails.json
 
 
-http get 'https://api.tukui.org/v1/addon/tukui' -b \
+curl https://api.tukui.org/v1/addon/tukui \
     | jq -r \
     > "$DIR"/tukui-ui--tukui.json
-http get 'https://api.tukui.org/v1/addon/elvui' -b \
+curl https://api.tukui.org/v1/addon/elvui \
     | jq -r \
     > "$DIR"/tukui-ui--elvui.json
 
 
-http get 'https://api.github.com/repos/nebularg/PackagerTest' -b \
+curl 'https://api.github.com/repos/nebularg/PackagerTest' \
     | jq -r \
     > "$DIR"/github-repo-release-json.json
-http get 'https://api.github.com/repos/nebularg/PackagerTest/releases?per_page=1' -b \
+curl 'https://api.github.com/repos/nebularg/PackagerTest/releases?per_page=1' \
     | jq -r \
     > "$DIR"/github-release-release-json.json
-http --follow get 'https://github.com/nebularg/PackagerTest/releases/download/v1.9.7/release.json' -b \
+curl 'https://github.com/nebularg/PackagerTest/releases/download/v1.9.7/release.json' \
+    -L \
     | jq -r \
     > "$DIR"/github-release-release-json-release-json.json
-http get 'https://api.github.com/repos/sfx-wow/masque' -b \
+curl 'https://api.github.com/repos/sfx-wow/masque' \
     | jq -r \
     > "$DIR"/github-repo-masque.json
-http get 'https://api.github.com/repos/sfx-wow/masque/releases?per_page=10' -b \
+curl 'https://api.github.com/repos/sfx-wow/masque/releases?per_page=10' \
     | jq -r \
     > "$DIR"/github-release-masque.json
-http --follow get $(
+curl $(
     jq -r \
         '.[] | select(.prerelease == false) | .assets[] | select(.name == "release.json") | .browser_download_url' \
         "$DIR"/github-release-masque.json
-) -b \
+    ) \
+    -L \
     | jq -r \
     > "$DIR"/github-release-masque-release-json.json
-http get 'https://api.github.com/repos/28/NoteworthyII' -b \
+curl 'https://api.github.com/repos/28/NoteworthyII' \
     | jq -r \
     > "$DIR"/github-repo-no-release-json.json
-http get 'https://api.github.com/repos/28/NoteworthyII/releases?per_page=1' -b \
+curl 'https://api.github.com/repos/28/NoteworthyII/releases?per_page=1' \
     | jq -r \
     > "$DIR"/github-release-no-release-json.json
-http get 'https://api.github.com/repos/AdiAddons/AdiBags' -b \
+curl 'https://api.github.com/repos/AdiAddons/AdiBags' \
     | jq -r \
     > "$DIR"/github-repo-no-releases.json
-http get 'https://api.github.com/repos/AdiAddons/AdiButtonAuras/releases/tags/2.0.19' -b \
+curl 'https://api.github.com/repos/AdiAddons/AdiButtonAuras/releases/tags/2.0.19' \
     | jq -r \
     > "$DIR"/github-release-no-assets.json
 
 
-http get https://raw.githubusercontent.com/layday/instawow-data/data/base-catalogue-v8.compact.json -b \
+curl https://raw.githubusercontent.com/layday/instawow-data/data/base-catalogue-v8.compact.json \
     | jq -r \
     > "$DIR"/base-catalogue-v8.compact.json
 
