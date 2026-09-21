4da9aaba42454e978326c86c62b15d3efadd5ff1
layday <layday@protonmail.com>
2026-09-21 18:42:44 +0100

    Use lazy imports in Python 3.15+; limit limports to the CLI

--- src/instawow/__init__.py
+++ src/instawow/__init__.py
@@ -1,7 +1,3 @@
 from __future__ import annotations
 
-from . import _import_wrapper
-
 NAME = __spec__.parent
-
-__getattr__ = _import_wrapper.make_getattr(NAME)
--- src/instawow/__main__.py
+++ src/instawow/__main__.py
@@ -1,6 +1,49 @@
 from __future__ import annotations
 
-from instawow.cli import main
+import sys
+
+
+def _make_getattr(package_name: str):
+    import importlib.util
+
+    def __getattr__(name: str):
+        """Defer importing own modules until attempting to access an attribute.
+
+        Importing this in ``__init__`` will overwrite relative imports.
+        """
+        fullname = package_name + '.' + name
+
+        try:
+            return sys.modules[fullname]
+        except KeyError:
+            spec = importlib.util.find_spec(fullname)
+            if spec is None or spec.loader is None:
+                # ``AttributeError`` is converted to an ``ImportError`` by the import machinery.
+                raise AttributeError from None
+
+            spec.loader = loader = importlib.util.LazyLoader(spec.loader)
+            sys.modules[fullname] = module = importlib.util.module_from_spec(spec)
+            loader.exec_module(module)
+
+            return module
+
+    return __getattr__
+
+
+def main():
+
+    if sys.version_info >= (3, 15):
+        sys.set_lazy_imports('all')
+
+    else:
+        import instawow
+
+        instawow.__getattr__ = _make_getattr(instawow.NAME)
+
+    from instawow.cli import main as main_
+
+    main_()
+
 
 if __name__ == '__main__':
     main()
